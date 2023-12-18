18db4336b651450aa2a75b305dc9be4e1b39bae6
Ikigai <alvardsandz@gmail.com>
2023-12-18 20:28:10 +0100

    add Orbitals & a cube in a cube

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // import * as THREE from "/node_modules/three/build/three.module.js";
 
+// import { PlaneGeometry } from "three/build/three.module";
 import {
   PerspectiveCamera,
   MeshStandardMaterial,
@@ -9,11 +10,9 @@ import {
   Mesh,
   BoxGeometry,
   AxesHelper,
-  
+  PlaneGeometry,
+  GridHelper
 } from "/node_modules/three/build/three.module.js";
-
-// import { OrbitControls } from "/node_modules/three/jsm/controls/OrbitControls.js";
-// import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
 
 import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
 
@@ -30,19 +29,28 @@ const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new BoxGeometry(1, 1, 1);
+const planeGeometry = new PlaneGeometry(5,5);
+const planeMaterial = new MeshBasicMaterial({color: 0xffffff});
+const plane = new Mesh(planeGeometry, planeMaterial);
+plane.rotation.x =  -0.5 * Math.PI
+// scene.add(plane)
+
+const geometry = new BoxGeometry(2, 2, 2);
 const material = new MeshBasicMaterial({
-    color: 0xda2e35,
+  color: 0xda2e35,
+  wireframe: true,
 });
 const cube = new Mesh(geometry, material);
 scene.add(cube);
 
-const axesHelper = new AxesHelper(5);
-scene.add(axesHelper);
+const geometryIn = new BoxGeometry(1, 1, 1);
+const materialIn = new MeshBasicMaterial({
+  color: 0xda2e35,
+  wireframe: true,
+});
+const cubeIn = new Mesh(geometryIn, materialIn);
+scene.add(cubeIn);
 
-// camera.position.z = 5;
-// camera.position.y = 1;
-// camera.position.x = 3;
 const orbit = new OrbitControls(camera, renderer.domElement);
 camera.position.set(0, 2, 5);
 orbit.update()
@@ -55,3 +63,17 @@ function animate() {
     renderer.render(scene, camera);
 }
 animate();
+
+function animateIn() {
+  cubeIn.rotation.x += 0.03;
+  cubeIn.rotation.y += 0.03;
+  requestAnimationFrame(animateIn);
+  renderer.render(scene, camera);
+}
+animateIn();
+
+const axesHelper = new AxesHelper(5);
+scene.add(axesHelper);
+
+const gridHelper = new GridHelper(5);
+scene.add(gridHelper);
